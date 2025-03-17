2a587002c0a5608c29c9da3d3963d5fec9bf3320
nhminh22@clc.fitus.edu.vn <nguyenhuuminh8204@gmail.com>
2025-03-17 13:05:03 +0700

    Initialize project

---
+++ Hacktok/app/src/main/java/com/hacktok/androidfinalproject/model/User.kt
@@ -0,0 +1,16 @@
+package com.hacktok.androidfinalproject.model
+
+import org.bson.types.ObjectId
+import java.util.Date
+
+data class User(
+    val id: ObjectId? = null,
+    val username: String,
+    val email: String,
+    val createdAt: Date = Date(),
+    val isActive: Boolean = true
+) {
+    override fun toString(): String {
+        return "id: $id, username: $username\n"
+    }
+}
