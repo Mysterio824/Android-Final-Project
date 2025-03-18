801116a3e309b41b08f2c08f4126733ac44a2dd5
nhminh22@clc.fitus.edu.vn <nguyenhuuminh8204@gmail.com>
2025-03-18 22:01:51 +0700

    11-Minh-create screen for friend list

---
+++ Hacktok/app/src/main/java/com/androidfinalproject/hacktok/ui/friendList/FriendListState.kt
@@ -0,0 +1,14 @@
+package com.androidfinalproject.hacktok.ui.friendList
+
+import com.androidfinalproject.hacktok.model.User
+import org.bson.types.ObjectId
+
+// UI State
+data class FriendListState(
+    val users: List<User> = emptyList(),
+    val filteredUsers: List<User> = emptyList(),
+    val friendIds: Set<ObjectId?> = emptySet(),
+    val searchQuery: String = "",
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
